553101580b1275cd91ea9130443e8fef0b6e37c6
Sobid <rustamovsobid95@gmail.com>
2023-10-24 15:23:16 +0500

    5-dars uchun topshiriq

---
+++ javascript/app.js
@@ -0,0 +1,5 @@
+let age = prompt("Yoshingiz nechada?");
+alert("Siz " + (2023 - age) + "-yilda tug'ilgansiz.");
+alert("Siz 3 yildan so'ng " + (+age + 3) + " yoshda bo'lasiz");
+let age_2 = prompt("Siz necha yoshgacha dasturlash bilan shug'illanmoqchisiz?");
+alert(" Siz yana " + (age_2 - age) + " yil dasturlash bilan shug'illanar ekansiz.");
